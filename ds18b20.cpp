4713c54f49fece6a2967d3065a4febcc0409d684
mengbishu <841616394@qq.com>
2018-06-20 16:42:09 +0800

    v1.0

--- ds18b20.cpp
+++ ds18b20.cpp
@@ -58,34 +58,36 @@ class microbitp : public MicroBitComponent
     }
 };
     
-//    MicroBitPin pin1 = uBit.io.P1;
+
 //    MicroBit uBit;
  //   MicroBitPin pin1 = uBit.io.P2;
 //    microbitp  pin0(7, 3, 15);
+    microbitp  pin0(7, MICROBIT_PIN_P0, PIN_CAPABILITY_ALL);
     microbitp  pin1(8, MICROBIT_PIN_P1, PIN_CAPABILITY_ALL);
-//    microbitp  pin2(9, 1, 15);
+    microbitp  pin2(9, MICROBIT_PIN_P2, PIN_CAPABILITY_ALL);
+    microbitp  pin = pin0;
 
     uint8_t init() {
-        pin1.setDigitalValue(0);
+        pin.setDigitalValue(0);
         for (volatile uint16_t i = 0; i < 600; i++);
-        pin1.setDigitalValue(1);
+        pin.setDigitalValue(1);
         for (volatile uint8_t i = 0; i < 30; i++);
-        int b = pin1.getDigitalValue();
+        int b = pin.getDigitalValue();
         for (volatile uint16_t i = 0; i < 600; i++);
         return b;
     }
 
     void sendZero() {
-        pin1.setDigitalValue(0);
+        pin.setDigitalValue(0);
         for (volatile uint8_t i = 1; i < 75; i++);
-        pin1.setDigitalValue(1);
+        pin.setDigitalValue(1);
         for (volatile uint8_t i = 1; i < 6; i++);
     }
 
     void sendOne() {
-        pin1.setDigitalValue(0);
+        pin.setDigitalValue(0);
         for (volatile uint8_t i = 1; i < 1; i++);
-        pin1.setDigitalValue(1);
+        pin.setDigitalValue(1);
         for (volatile uint8_t i = 1; i < 80; i++);
     }
 
@@ -98,9 +100,9 @@ class microbitp : public MicroBitComponent
             delay1 = 75;
             delay2 = 6;
         }
-        pin1.setDigitalValue(0);
+        pin.setDigitalValue(0);
         for (uint8_t i = 1; i < delay1; i++);
-        pin1.setDigitalValue(1);
+        pin.setDigitalValue(1);
         for (uint8_t i = 1; i < delay2; i++);
     }
 
@@ -129,10 +131,10 @@ class microbitp : public MicroBitComponent
 
     int readBit() {
         volatile int i;
-        pin1.setDigitalValue(0);
-        pin1.setDigitalValue(1);
+        pin.setDigitalValue(0);
+        pin.setDigitalValue(1);
         for (i = 1; i < 20; i++);
-        int b = pin1.getDigitalValue();
+        int b = pin.getDigitalValue();
         for (i = 1; i < 60; i++);
         return b;
     }
@@ -160,8 +162,13 @@ class microbitp : public MicroBitComponent
     }
 
     //%
-    int16_t Temperature() {
-      
+    int16_t Temperature(int p) {
+        if(p == 0)
+          pin = pin0
+        else if(p == 1)
+          pin = pin1
+        else if(p = 2)
+          pin = pin2
         init();
         writeByte(0xCC);
         convert();
--- ds18b20.ts
+++ ds18b20.ts
@@ -10,23 +10,32 @@ namespace DS18B20 {
      * block="Temperature(C)"
      */
 
+     enum pin {
+       //% block=pin0
+       pin0 = 0,
+       //% block=pin1
+       pin1 = 1,
+       //% block=pin2
+       pin2 = 2
+     }
+     
     //% shim=DS18B20::Temperature
-    export function Temperature(): number {
+    export function Temperature(p : number): number {
         // Fake function for simulator
         return 0
     }
     
     //% weight=10 blockId="Temperature_number" 
-    //% block="Temperature_number"
-    export function Temperature_number(): number {
+    //% block="Temperature_number %p"
+    export function Temperature_number(p : pin): number {
         // Fake function for simulator
-        return Temperature()
+        return Temperature(p)
     }
     
     //% weight=10 blockId="Temperature_string" 
-    //% block="Temperature_string"
-    export function Temperature_string() : string{
-        let temp = Temperature();
+    //% block="Temperature_string %p"
+    export function Temperature_string(p : pin) : string{
+        let temp = Temperature(p);
         let x = (temp / 100)
         let y = (temp % 100)
         let z = ''
